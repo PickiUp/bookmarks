79849da7c6440e87065b67679d8b3d8a3e5b9faa
PickiUp <lzy2001@proton.me>
2024-01-21 16:48:26 +0800

    基本布局

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,261 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import React, { useState } from 'react';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    UserOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme, Input } from 'antd';
+const { Search } = Input;
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
 }
-
-export default App
+const items = [
+    getItem('Option 1', '1', <PieChartOutlined />),
+    getItem('Option 2', '2', <DesktopOutlined />),
+    getItem('User', 'sub1', <UserOutlined />, [
+        getItem('Tom', '3'),
+        getItem('Bill', '4'),
+        getItem('Alex', '5'),
+    ]),
+    getItem('Team', 'sub2', <TeamOutlined />, [
+        getItem('Team 1', '6'),
+        getItem('Team 2', '8'),
+    ]),
+    getItem('Files', '9', <FileOutlined />),
+];
+const App = () => {
+    const [collapsed, setCollapsed] = useState(false);
+    const [siderWidth, setSiderWidth] = useState('200');
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+    const onSearch = (value, _e, info) => console.log(info?.source, value);
+    function handleCollapse(value) {
+        setCollapsed(value);
+        value ? setSiderWidth('80') : setSiderWidth('200');
+    }
+    return (
+        <Layout
+            style={{
+                minHeight: '100vh',
+            }}>
+            <Sider
+                style={{
+                    overflow: 'auto',
+                    height: '100vh',
+                    position: 'fixed',
+                    left: 0,
+                    top: 0,
+                    bottom: 0,
+                }}
+                collapsible
+                collapsed={collapsed}
+                onCollapse={handleCollapse}>
+                <Search
+                    placeholder="input search text"
+                    onSearch={onSearch}
+                    enterButton
+                    onClick={() => {
+                        setCollapsed(false);
+                        setSiderWidth('200');
+                    }}
+                />
+                <Menu
+                    theme="dark"
+                    defaultSelectedKeys={['1']}
+                    mode="inline"
+                    items={items}
+                />
+            </Sider>
+            <Layout
+                style={{
+                    width: `calc(100vw - ${siderWidth}px)`,
+                    marginLeft: `${siderWidth}px`,
+                }}>
+                <Header
+                    style={{
+                        padding: 0,
+                        background: colorBgContainer,
+                    }}
+                />
+                <Content
+                    style={{
+                        margin: '0 16px',
+                    }}>
+                    <Breadcrumb
+                        style={{
+                            margin: '16px 0',
+                        }}>
+                        <Breadcrumb.Item>User</Breadcrumb.Item>
+                        <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div
+                        style={{
+                            padding: 24,
+                            minHeight: 360,
+                            background: colorBgContainer,
+                            borderRadius: borderRadiusLG,
+                            color: '#000',
+                        }}>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Adipisci aut reiciendis eos labore distinctio
+                            laboriosam obcaecati nihil omnis reprehenderit
+                            dignissimos, vel voluptate, fugiat inventore
+                            necessitatibus tenetur sapiente laudantium cum. Ut.
+                        </p>
+                    </div>
+                </Content>
+                <Footer
+                    style={{
+                        textAlign: 'center',
+                        width: '100%',
+                    }}>
+                    Ant Design ©{new Date().getFullYear()} Created by Ant UED
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+export default App;
